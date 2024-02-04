7d2e19957c378e77f194125bec481f85b9d13fc6
Cocoflosbach <84343628+Cocoflosbach@users.noreply.github.com>
2024-02-04 12:23:37 +0100

    Update users schema model

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    Username: { type: String, required: true},
-    Password: { type: String, required: true},
-    Email: { type: String, required: true},
-    Birthday: Date,
-    PhoneNo: { type: String, required: true},
-    Address: { type: String, required: true},
-    FavoriteProducts: [{ type: mongoose.Schema.Types.ObjectId, ref: "product"}],
-    WishList: [{ type: mongoose.Schema.Types.ObjectId, ref: "product"}],
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: {type: Date},
+    FavoriteProducts: [{ type: mongoose.Schema.Types.ObjectId, ref: "product"}]
 })
 
-module.exports = mongoose.model("Users", userSchema)
+module.exports = mongoose.model("Users", userSchema);
